6e73b873a6703f4ff74363923d8072248b734df1
Daniele Albanese <daniele.albanese98@gmail.com>
2021-01-10 01:45:59 +0100

    Added subRoutes to main routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const attack = require("./subRoutes/attack")
+// const device = require("./subRoutes/device")
+const healthCheck = require("./subRoutes/healthCheckl");
+const payload = require("./subRoutes/payload");
+
+router.use("/attack", attack);
+// router.use("/device", device);
+router.use("/health_check", healthCheck);
+router.use("/payload", payload);
+
+router.route("*")
+    .get((req, res) => {
+        console.log("GET - Fall back");
+
+        res.status(404)
+            .json({message: "Route Not Found"});
+    })
+    .post((req, res) => {
+        console.log("POST - Fall back");
+
+        res.status(404)
+            .json({message: "Route Not Found"});
+    })
+    .delete((req, res) => {
+        console.log("DELETE - Fall back");
+
+        res.status(404)
+            .json({message: "Route Not Found"});
+    })
+    .put((req, res) => {
+        console.log("PUT - Fall back");
+
+        res.status(404)
+            .json({message: "Route Not Found"});
+    })
 
 module.exports = router;
