682682bcfb27d6e47a7807a8709c0571422cdf86
Daniele Albanese <daniele.albanese98@gmail.com>
2021-04-05 00:38:56 +0200

    Added config.json + Added script env_file_creator.sh

---
+++ scripts/env_file_creator.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CONFIG="config.json"
+#PATH=$(pwd)
+
+cd .. || exit
+
+if [ -f ".env" ] ;
+  then
+    rm .env
+  fi
+
+touch .env
+
+{
+  echo "SERVER_PORT=$(jq .backend.SERVER_PORT $CONFIG)"
+
+  echo "MONGO_INITDB_DATABASE=$(jq .db.MONGO_INITDB_DATABASE $CONFIG)"
+  echo "MONGO_INITDB_ROOT_USERNAME=$(jq .db.MONGO_INITDB_ROOT_USERNAME $CONFIG)"
+  echo "MONGO_INITDB_ROOT_PASSWORD=$(jq .db.MONGO_INITDB_ROOT_PASSWORD $CONFIG)"
+  echo "MONGO_PORT=$(jq .db.MONGO_PORT $CONFIG)"
+  echo "MONGO_AUTH_SOURCE=$(jq .db.MONGO_AUTH_SOURCE $CONFIG)"
+} >> .env
+
+exit 0
