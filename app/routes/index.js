cfb785d4c39a96e2f8617a944dfdd3005f719e9e
Daniele Albanese <daniele.albanese98@gmail.com>
2021-04-05 00:37:42 +0200

    routes -> Deleted route healthCheckl.js + Updated index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const attack = require('./subRoutes/attack');
 // const device = require('./subRoutes/device');
-const healthCheck = require('./subRoutes/healthCheckl');
 const payload = require('./subRoutes/payload');
 
 const HEALTH_CHECK_ENDPOINT = '/health_check';
@@ -11,33 +10,13 @@ const PAYLOAD_ENDPOINT = '/payload';
 const ATTACK_ENDPOINT = '/attack';
 // const DEVICE_ENDPOINT = '/device';
 
-router.route('*')
-    .get((req, res) => {
-        console.log('GET - Fall back');
+router.get(HEALTH_CHECK_ENDPOINT, function (req, res, next) {
+    res.status(200)
+        .json({
+            message: 'It working...'
+        });
+})
 
-        res.status(404)
-            .json({message: 'Route Not Found'});
-    })
-    .post((req, res) => {
-        console.log('POST - Fall back');
-
-        res.status(404)
-            .json({message: 'Route Not Found'});
-    })
-    .delete((req, res) => {
-        console.log('DELETE - Fall back');
-
-        res.status(404)
-            .json({message: 'Route Not Found'});
-    })
-    .put((req, res) => {
-        console.log('PUT - Fall back');
-
-        res.status(404)
-            .json({message: 'Route Not Found'});
-    })
-
-router.use(HEALTH_CHECK_ENDPOINT, healthCheck);
 router.use(PAYLOAD_ENDPOINT, payload);
 router.use(ATTACK_ENDPOINT, attack);
 // router.use(DEVICE_ENDPOINT, device);
