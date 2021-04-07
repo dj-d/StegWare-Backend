c544bd424cd20177e72effd9ac1a9b5476fc8f57
Daniele Albanese <daniele.albanese98@gmail.com>
2021-04-08 00:30:13 +0200

    app.js -> Updated cors options

--- app/app.js
+++ app/app.js
@@ -16,18 +16,27 @@ app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(compression());
-app.use(helmet())
+app.use(helmet());
 
 const PORT = process.env.SERVER_PORT || 3333;
-const FRONTEND_ORIGINS = process.env.FRONTEND_ORIGINS || 'localhost';
+const FRONTEND_ORIGINS = process.env.FRONTEND_ORIGINS.split(',') || 'localhost';
+// const FRONTEND_PORT = process.env.FRONTEND_PORT || '3000';
+let ORIGINS = [];
+
+for (let i = 0; i < FRONTEND_ORIGINS.length; i++) {
+    let origin = ('http://' + FRONTEND_ORIGINS[i])
+    origin = origin.replace(/\s/g, '');
+    ORIGINS.push(origin);
+}
 
 let corsOptions = {
     allowedHeaders: ['Content-Type'],
     preflightContinue: true,
-    origin: FRONTEND_ORIGINS
+    origin: ORIGINS
 }
 
 app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
