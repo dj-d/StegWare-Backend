54a91df47d56786a6970240a7cca2ad118f98bc6
Daniele Albanese <daniele.albanese98@gmail.com>
2021-04-08 00:32:42 +0200

    Updated databse config

--- app/database/config/dbConfig.js
+++ app/database/config/dbConfig.js
@@ -5,7 +5,7 @@ let dbDefaultCredential = require('./dbDefaultCredential.json');
 const username = process.env.MONGO_INITDB_ROOT_USERNAME || dbDefaultCredential.db.username;
 const password = process.env.MONGO_INITDB_ROOT_PASSWORD || dbDefaultCredential.db.password;
 const port = process.env.MONGO_PORT || dbDefaultCredential.db.port;
-const host = process.env.DOCKER_RUNNING ? 'mongo' : dbDefaultCredential.db.host;
+const host = process.env.MONGO_HOST || dbDefaultCredential.db.host;
 const dbName = process.env.MONGO_INITDB_DATABASE || dbDefaultCredential.db.name;
 const authSource = process.env.MONGO_AUTH_SOURCE || dbDefaultCredential.db.authSource;
 
