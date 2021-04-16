90d04d9d28461d6eab5e2e9b3107646b58dbb5da
dj-d <nicodani>
2021-04-17 01:52:33 +0200

    Payload -> models -> Updated all query with promises + Added DOC

--- app/database/models/Payload.js
+++ app/database/models/Payload.js
@@ -10,22 +10,22 @@ const payloadError = require('../../constants/payloadError')
  * @returns {Promise<unknown>}
  */
 async function isPayloadOfDB(payload_id) {
-    return new Promise((resolve, reject) => {
-        payloadModel.findOne({'_id': payload_id}, (error, payloadData) => {
-            if (payloadData !== null) {
-                console.log('Payload - isPayloadOfDB - True');
-                resolve(true);
-            } else {
-                if (!error) {
-                    console.log('Payload - isPayloadOfDB - False');
-                    resolve(false);
-                } else {
-                    console.log('Payload - isPayloadOfDB - Error: ' + error);
-                    reject(error);
-                }
-            }
-        })
-    })
+	return new Promise((resolve, reject) => {
+		payloadModel.findOne({'_id': payload_id}, (error, payloadData) => {
+			if (payloadData !== null) {
+				console.log('Payload - isPayloadOfDB - True');
+				resolve(true);
+			} else {
+				if (!error) {
+					console.log('Payload - isPayloadOfDB - False');
+					resolve(false);
+				} else {
+					console.log('Payload - isPayloadOfDB - Error: ' + error);
+					reject(error);
+				}
+			}
+		})
+	})
 }
 
 /**
@@ -35,119 +35,151 @@ async function isPayloadOfDB(payload_id) {
  * @returns {*}
  */
 async function create(payloadData) {
-    const alreadyExist = await isPayloadOfDB(payloadData);
+	const alreadyExist = await isPayloadOfDB(payloadData);
 
-    return new Promise((resolve, reject) => {
-        if (!alreadyExist) {
-            const payload = new payloadModel(payloadData);
+	return new Promise((resolve, reject) => {
+		if (!alreadyExist) {
+			const payload = new payloadModel(payloadData);
 
-            payload.save((error, data) => {
-                if (data !== null) {
-                    console.log('Payload - create - Saved');
-                    resolve(true);
-                } else {
-                    if (!error) {
-                        console.log('Payload - create - Not Saved');
-                        resolve(false);
-                    } else {
-                        console.log('Payload - create - Error: ' + error);
-                        reject(error);
-                    }
-                }
-            })
-        } else {
-            console.log('Payload already exist');
-            reject(payloadError.ERROR);
-        }
-    });
+			payload.save((error, data) => {
+				if (data !== null) {
+					console.log('Payload - create - Saved');
+					resolve(true);
+				} else {
+					if (!error) {
+						console.log('Payload - create - Not Saved');
+						resolve(false);
+					} else {
+						console.log('Payload - create - Error: ' + error);
+						reject(error);
+					}
+				}
+			})
+		} else {
+			console.log('Payload already exist');
+			reject(payloadError.ERROR);
+		}
+	});
 
 }
 
 /**
+ * Get specific payload by id
  *
  * @param payload_id
  * @returns {*}
  */
 function getById(payload_id) {
-    const isValidPayloadID = isPayloadOfDB(payload_id);
+	const isValidPayloadID = isPayloadOfDB(payload_id);
 
-    return new Promise((resolve, reject) => {
-        if (isValidPayloadID) {
-            payloadModel.findById(payload_id, (error, payloadData) => {
-                if (payloadData !== null) {
-                    const payload = new payloadModel(payloadData);
-                    resolve(payload);
-                } else {
-                    if (!error) {
-                        console.log('Payload - readOneById - Not data');
-                        resolve(payloadError.ERROR);
-                    } else {
-                        console.log('Payload - readOneById - Error: ' + error);
-                        reject(error);
-                    }
-                }
-            })
-        } else {
-            console.log('Payload not exist');
-            resolve(payloadError.ERROR);
-        }
-    });
+	return new Promise((resolve, reject) => {
+		if (isValidPayloadID) {
+			payloadModel.findById(payload_id, (error, payloadData) => {
+				if (payloadData !== null) {
+					const payload = new payloadModel(payloadData);
+
+					resolve(payload);
+				} else {
+					if (!error) {
+						console.log('Payload - readOneById - Not data');
+						resolve(payloadError.ERROR);
+					} else {
+						console.log('Payload - readOneById - Error: ' + error);
+						reject(error);
+					}
+				}
+			})
+		} else {
+			console.log('Payload not exist');
+			reject(payloadError.ERROR);
+		}
+	});
 }
 
 /**
+ * Get all payloads
  *
  * @returns {*}
  */
 async function getAll() {
-    return new Promise((resolve, reject) => {
-        payloadModel.find({}, (error, res) => {
-            if (res !== null) {
-                resolve(res);
-            } else {
-                if (!error) {
-                    resolve(false);
-                } else {
-                    reject(error)
-                }
-            }
-        })
-    })
+	return new Promise((resolve, reject) => {
+		payloadModel.find({}, (error, res) => {
+			if (res !== null) {
+				resolve(res);
+			} else {
+				if (!error) {
+					resolve(false);
+				} else {
+					reject(error)
+				}
+			}
+		})
+	})
 }
 
-// TODO: To complete
 /**
+ * Remove specific payload from DB by id
  *
  * @param payload_id
  * @returns {*}
  */
 function remove(payload_id) {
-    return payloadModel.deleteOne(
-        {
-            _id: payload_id
-        }).lean().exec();
+	const alreadyExist = isPayloadOfDB(payload_id);
+
+	return new Promise(((resolve, reject) => {
+		if (alreadyExist) {
+			resolve(
+				payloadModel.deleteOne(
+					{
+						_id: payload_id
+					}
+				)
+					.lean()
+					.exec()
+			);
+		} else {
+			console.log('Payload not exist');
+			reject(payloadError.ERROR);
+		}
+	}));
 }
 
-// TODO: To complete
+
 /**
+ * Update data of specific payload by id
  *
  * @param payload_id
  * @param update_payload
  * @returns {*}
  */
 function update(payload_id, update_payload) {
-    return payloadModel.updateOne(
-        {
-            _id: payload_id
-        },
-        {
-            $set: update_payload
-        }).lean().exec();
+	const alreadyExist = isPayloadOfDB(payload_id);
+
+	return new Promise((resolve, reject) => {
+		if (alreadyExist) {
+			resolve(
+				payloadModel.updateOne(
+					{
+						_id: payload_id
+					},
+					{
+						$set: update_payload
+					}
+				)
+					.lean()
+					.exec()
+			);
+		} else {
+			console.log('Payload not exist');
+			reject(payloadError.ERROR);
+		}
+	})
 }
 
 module.exports = {
-    create,
-    getAll,
-    getById,
-    remove,
-    update
+	create,
+	getAll,
+	getById,
+	remove,
+	update
 };
