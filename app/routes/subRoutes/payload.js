fe31efe3386a7602527a529d0aec407d22021b4a
dj-d <nicodani>
2021-04-17 01:54:24 +0200

    Payload -> routes -> Added DOC + Updated function with async/await

--- app/routes/subRoutes/payload.js
+++ app/routes/subRoutes/payload.js
@@ -4,84 +4,99 @@ const router = express.Router();
 const payload = require('../../database/models/Payload');
 const httpStatusCode = require('../../constants/httpStatusCode');
 
+/**
+ * Get payload data
+ */
 router.get('/', async (req, res) => {
-    const payload_id = req.query.payload_id;
+	const payload_id = req.query.payload_id;
 
-    if (payload_id) {
-        payload.readOneById(payload_id)
-            .then((payload) => {
-                if (payload) {
-                    res.json(payload);
-                } else {
-                    res.status(httpStatusCode.NOT_FOUND)
-                        .json({
-                            error: 'Payload not found'
-                        });
-                }
-            })
-            .catch((error) => {
-                res.status(error.status)
-                    .json({
-                        error: error.message
-                    });
-            })
-    } else {
-        await payload.readAll()
-            .then((payload) => {
-                res.status(httpStatusCode.OK).json(payload);
-            })
-            .catch((error) => {
-                res.status(error.status)
-                    .json({
-                        error: error.message
-                    });
-            })
-    }
+	if (payload_id) {
+		await payload.getById(payload_id)
+			.then((payload) => {
+				if (payload) {
+					res
+						.status(httpStatusCode.OK)
+						.json(payload);
+				} else {
+					res
+						.status(httpStatusCode.NOT_FOUND)
+						.json({
+							error: 'Payload not found'
+						});
+				}
+			})
+			.catch((error) => {
+				res.status(error.status)
+					.json({
+						error: error.message
+					});
+			})
+	} else {
+		await payload.getAll()
+			.then((payload) => {
+				res.status(httpStatusCode.OK).json(payload);
+			})
+			.catch((error) => {
+				res.status(error.status)
+					.json({
+						error: error.message
+					});
+			})
+	}
 
-    res.end();
+	res.end();
 });
 
-router.post('/', (req, res) => {
-    payload.create(req.body)
-        .then((payload) => {
-            res.json(payload);
-        })
-        .catch((error) => {
-            res.status(httpStatusCode.INTERNAL_SERVER_ERROR)
-                .json({
-                    error: error
-                });
-        })
+/**
+ * Add payload into the DB
+ */
+router.post('/', async (req, res) => {
+	await payload.create(req.body)
+		.then((payload) => {
+			res.json(payload);
+		})
+		.catch((error) => {
+			res.status(httpStatusCode.INTERNAL_SERVER_ERROR)
+				.json({
+					error: error
+				});
+		})
 })
 
-router.delete('/', (req, res) => {
-    const payload_id = req.query.payload_id;
+/**
+ * Remove payload from DB
+ */
+router.delete('/', async (req, res) => {
+	const payload_id = req.query.payload_id;
 
-    payload.deleteOne(payload_id)
-        .then((payload) => {
-            res.json(payload);
-        })
-        .catch((error) => {
-            res.status(error.status)
-                .json({
-                    error: error.message
-                });
-        })
+	await payload.remove(payload_id)
+		.then((payload) => {
+			res.json(payload);
+		})
+		.catch((error) => {
+			res.status(error.status)
+				.json({
+					error: error.message
+				});
+		})
 })
 
-router.put('/', (req, res) => {
-    const payload_id = req.query.payload_id;
+/**
+ * Update specific payload
+ */
+router.put('/', async (req, res) => {
+	const payload_id = req.query.payload_id;
 
-    payload.updateOne(payload_id, req.body)
-        .then((payload) => {
-            res.json(payload);
-        })
-        .catch((error) => {
-            res.status(httpStatusCode.INTERNAL_SERVER_ERROR)
-                .json({
-                    error: error
-                });
-        })
+	await payload.update(payload_id, req.body)
+		.then((payload) => {
+			res.json(payload);
+		})
+		.catch((error) => {
+			res.status(httpStatusCode.INTERNAL_SERVER_ERROR)
+				.json({
+					error: error
+				});
+		})
 })
 
 module.exports = router;
