a5148d378f72fb35faf0e0c9c4d838d0e3e2a01f
dj-d <nicodani>
2021-04-18 17:28:51 +0200

    routes/subRoutes/attack -> Fixed code smell + Added httpStatusCode + Reformatting

--- app/routes/subRoutes/attack.js
+++ app/routes/subRoutes/attack.js
@@ -1,39 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-const attack = require("../../database/models/Attack")
+const attack = require("../../database/models/Attack");
+const httpStatusCode = require('../../constants/httpStatusCode');
+
+// TODO: Ask to ZappaBoy
+function errorResponse(res, error) {
+    return res.status(error.status)
+        .json({
+            error: error.message
+        });
+}
 
 router.get('/', async (req, res) => {
     const attack_id = req.query.attack_id;
 
     if (attack_id) {
         await attack.getById(attack_id)
-            .then((attack) => {
-                if (attack) {
-                    res.json(attack);
+            .then((attackData) => {
+                if (attackData) {
+                    res.status(httpStatusCode.OK)
+                        .json(attackData);
                 } else {
-                    res.status(404)
+                    res.status(httpStatusCode.NOT_FOUND)
                         .json({
                             error: 'Payload not found'
                         });
                 }
             })
             .catch((error) => {
-                res.status(error.status)
-                    .json({
-                        error: error.message
-                    });
+                errorResponse(res, error);
             })
     } else {
         await attack.getAll()
-            .then((attack) => {
-                res.json(attack);
+            .then((attackData) => {
+                res.status(httpStatusCode.OK)
+                    .json(attackData);
             })
             .catch((error) => {
-                res.status(error.status)
-                    .json({
-                        error: error.message
-                    });
+                errorResponse(res, error);
             })
     }
 });
@@ -42,14 +47,12 @@ router.delete('/', async (req, res) => {
     const attack_id = req.query.attack_id;
 
     await attack.remove(attack_id)
-        .then((attack) => {
-            res.json(attack);
+        .then((attackData) => {
+            res.status(httpStatusCode.OK)
+                .json(attackData);
         })
         .catch((error) => {
-            res.status(error.status)
-                .json({
-                    error: error.message
-                });
+            errorResponse(res, error);
         })
 })
 
