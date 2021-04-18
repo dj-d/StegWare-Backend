2d980f26943fde155bf040ac874ae6edf07ea286
dj-d <nicodani>
2021-04-18 23:35:26 +0200

    routes/subRoutes/attack -> Added DOC + Added function dataNotFound + Added missed 'res.end()'

--- app/routes/subRoutes/attack.js
+++ app/routes/subRoutes/attack.js
@@ -6,54 +6,76 @@ const httpStatusCode = require('../../constants/httpStatusCode');
 
 // TODO: Ask to ZappaBoy
 function errorResponse(res, error) {
-    return res.status(error.status)
-        .json({
-            error: error.message
-        });
+	return res.status(error.status)
+		.json({
+			error: error.message
+		});
 }
 
-router.get('/', async (req, res) => {
-    const attack_id = req.query.attack_id;
+function dataNotFound(res) {
+	return res.status(httpStatusCode.NOT_FOUND)
+		.json({
+			error: 'Attack not found'
+		});
+}
 
-    if (attack_id) {
-        await attack.getById(attack_id)
-            .then((attackData) => {
-                if (attackData) {
-                    res.status(httpStatusCode.OK)
-                        .json(attackData);
-                } else {
-                    res.status(httpStatusCode.NOT_FOUND)
-                        .json({
-                            error: 'Payload not found'
-                        });
-                }
-            })
-            .catch((error) => {
-                errorResponse(res, error);
-            })
-    } else {
-        await attack.getAll()
-            .then((attackData) => {
-                res.status(httpStatusCode.OK)
-                    .json(attackData);
-            })
-            .catch((error) => {
-                errorResponse(res, error);
-            })
-    }
+/**
+ * Get attack data
+ */
+router.get('/', async (req, res) => {
+	const attack_id = req.query.attack_id;
+
+	if (attack_id) {
+		await attack.getById(attack_id)
+			.then((attackData) => {
+				if (attackData) {
+					res.status(httpStatusCode.OK)
+						.json(attackData);
+				} else {
+					dataNotFound(res);
+				}
+			})
+			.catch((error) => {
+				errorResponse(res, error);
+			})
+	} else {
+		await attack.getAll()
+			.then((attackData) => {
+				if (attackData) {
+					res.status(httpStatusCode.OK)
+						.json(attackData);
+				} else {
+					dataNotFound(res);
+				}
+			})
+			.catch((error) => {
+				errorResponse(res, error);
+			})
+	}
+
+	res.end();
 });
 
+/**
+ * Remove specific attack
+ */
 router.delete('/', async (req, res) => {
-    const attack_id = req.query.attack_id;
+	const attack_id = req.query.attack_id;
 
-    await attack.remove(attack_id)
-        .then((attackData) => {
-            res.status(httpStatusCode.OK)
-                .json(attackData);
-        })
-        .catch((error) => {
-            errorResponse(res, error);
-        })
+	await attack.remove(attack_id)
+		.then((attackData) => {
+			if (attackData) {
+				res.status(httpStatusCode.OK)
+					.json(attackData);
+			} else {
+				dataNotFound(res);
+			}
+		})
+		.catch((error) => {
+			errorResponse(res, error);
+		})
+
+	res.end();
 })
 
 module.exports = router;
