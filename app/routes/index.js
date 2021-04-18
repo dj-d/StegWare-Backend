78fda4c97f4933e4d403b92bfb952d8170d9bd1d
dj-d <nicodani>
2021-04-18 17:08:51 +0200

    routes/index -> Fixed code smell

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const attack = require('./subRoutes/attack');
-// const device = require('./subRoutes/device');
+const device = require('./subRoutes/device');
 const payload = require('./subRoutes/payload');
 
 const HEALTH_CHECK_ENDPOINT = '/health_check';
 const PAYLOAD_ENDPOINT = '/payload';
 const ATTACK_ENDPOINT = '/attack';
-// const DEVICE_ENDPOINT = '/device';
+const DEVICE_ENDPOINT = '/device';
 
-router.get(HEALTH_CHECK_ENDPOINT, function (req, res, next) {
+router.get(HEALTH_CHECK_ENDPOINT, function (req, res) {
     res.status(200)
         .json({
             message: 'It working...'
@@ -19,6 +19,6 @@ router.get(HEALTH_CHECK_ENDPOINT, function (req, res, next) {
 
 router.use(PAYLOAD_ENDPOINT, payload);
 router.use(ATTACK_ENDPOINT, attack);
-// router.use(DEVICE_ENDPOINT, device);
+router.use(DEVICE_ENDPOINT, device);
 
 module.exports = router;
